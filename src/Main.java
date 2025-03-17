65fec3816c2a95567a76c032c3139c144793352a
nilsedgar <nils.jacobsen@zocom.se>
2025-03-17 12:19:07 +0100

    ny branchmeddelande

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Jag heter Nils och detta är min branch");
     }
 }
